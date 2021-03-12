00477187124408c3845cd2c0b8b5c11011bed1bf
fdebrabant <debrabant.fanny@gmail.com>
2021-03-12 18:10:09 +0100

    add book by id route

--- routes/book.controller.js
+++ routes/book.controller.js
@@ -15,4 +15,20 @@ router.get('/', function(request, response) {
     });
 });
 
+// SHOW BOOK BY ID
+
+router.get('/:id', function(request, response) {
+    connection.query('SELECT * FROM book WHERE id = ?',
+    [request.params.id],
+    (err,result) => {
+        if (err) {
+            response.status(500).json(err);
+        } else if (result.length === 0) {
+            response.sendStatus(404);
+        } else {
+            response.status(200).json(result[0]);
+        }
+    });
+});
+
 module.exports = router;
